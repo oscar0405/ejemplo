e8d1a240154ccab33dfdac87039cba046729e513
Oscar Hernandez <joss336699@gmail.com>
2017-07-25 12:51:05 -0500

    first commit

---
+++ s1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo " inicia el script 1" ;
+ls . ;
+mkdir magia ;
+touch alibaba
+echo " Hocus Pocus" > ./alibaba ;
+cat ./alibaba ;
+echo "Fin del script 1" ;
---
+++ script2
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "iniciar el script 2" ;
+./s1.sh ;
+touch $1 ;
+ls . ;
+mv ./$1 ./$2 ;
+echo "Fin del script 2"
